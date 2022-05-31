17a4979c5aec75a45318659cd1892d98729f83cf
NatellaVerde <xxxxnataliyaxxxx@gmail.com>
2022-05-31 22:49:02 +0300

    search layout

--- src/app/components/apartment/Apartment.jsx
+++ src/app/components/apartment/Apartment.jsx
@@ -19,37 +19,44 @@ const Apartment = ({userId}) => {
             {flat && <div className={s.container}>
                 <div className={s.apart}>
                     <div className={s.apart_info}>
-                        <div className={s.apart_info_data}>
-                            <div className={s.apart_info_person}>
-                                <b>Информация о арендодателе (рейтинг/имя)</b>
-                                {flat.rate}/{flat.user}
+                        <div className={s.apart_left}>
+                            <div className={s.apart_info_data}>
+                                <div className={s.apart_info_person}>
+                                    <b> Информация о арендодателе (рейтинг/имя): </b>
+                                   <br/> {flat.rate} / {flat.user}
+                                </div>
+                                <div className={s.apart_info_price}>
+                                    <b>Информация о цене за время пребывания: </b>
+                                    <br/> {flat.cost}
+                                </div>
+                                <div className={s.apart_info_button}>
+                                    <button>Забронировать</button>
+                                </div>
                             </div>
-                            <div className={s.apart_info_search}>
-                                Инофрмация о поиске
-                            </div>
-                            <div className={s.apart_info_price}>
-                                <b>Информация о цене за время пребывания: </b>
-                                {flat.cost}
-                            </div>
-                            <div className={s.apart_info_button}>
-                                <button>Забронировать</button>
+                            <div className={s.apart_info_mainpic}>
+                                <Zoom>
+                                    <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/1.jpg`)} alt="Значок" width={"200px"}/>
+                                </Zoom>
                             </div>
                         </div>
-                        <Zoom>
-                            <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/1.jpg`)} alt="Значок" width={"200px"}/>
-                        </Zoom>
-                        <Zoom>
-                            <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/2.jpg`)} alt="Значок" width={"200px"}/>
-                        </Zoom>
-                        <Zoom>
-                            <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/3.jpg`)} alt="Значок" width={"200px"}/>
-                        </Zoom>
+                        <div className={s.apart_right}>
+                            <div className={s.apart_info_pic}>
+                                <Zoom>
+                                    <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/2.jpg`)} alt="Значок" width={"200px"}/>
+                                </Zoom>
+                            </div>
+                            <div className={s.apart_info_pic}>
+                                <Zoom>
+                                    <img className={s.apart_info_image} src={require(`../../assets/img/flats/${userId}/3.jpg`)} alt="Значок" width={"200px"}/>
+                                </Zoom>
+                            </div>
+                        </div>
                     </div>
                     <div className={s.apart_description}>
                         <b>тут идет описание:</b> {flat.description}
                     </div>
                     <div className={s.apart_reviews}>
-                        <b>отзывы об апартамент: </b>
+                        <b> отзывы об апартаментах: </b>
                         {flat.review.map(r => (
                             <div key={r}>{r}</div>
                         ))}
